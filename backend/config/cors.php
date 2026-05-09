3ccbdcca76a4ea2c92082ef14c99352702d3b4f5
Job <chirinosjob@gmail.com>
2026-05-09 14:09:27 -0400

    cors errors fixed

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://cesaraugustoapp.onrender.com'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://cesar-augusto-app.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
